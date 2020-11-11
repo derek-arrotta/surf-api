1049ec8416b1495aecb8274b93380311affa971b
Derek Arrotta <derek.arrotta@gmail.com>
2020-11-11 13:50:57 -0500

    old junk code (could be useful in future)

---
+++ oldJunkCode.js
@@ -0,0 +1,38 @@
+// time format
+  /*
+  const year = new Date(timeUTCms).getUTCFullYear(); // year
+  const month = new Date(timeUTCms).getUTCMonth() + 1; // month (january is 0)
+  const day = new Date(timeUTCms).getUTCDate(); // day
+  const hours = new Date(timeUTCms).getUTCHours(); // hours
+  const minutes = new Date(timeUTCms).getUTCMinutes(); // minutes
+  const seconds = new Date(timeUTCms).getUTCSeconds(); // seconds
+  const milliseconds = new Date(timeUTCms).getUTCMilliseconds(); // milliseconds
+  console.log(year);
+  console.log(month);
+  console.log(day);
+  console.log(hours);
+  console.log(minutes);
+  console.log(seconds);
+  console.log(milliseconds);
+  */
+ //let timeFormatted4 = ''; // milliseconds (wasn't working in sg fetch url so i got rid of it)
+   //for (let i=20; i<22; i++) {
+  //  timeFormatted4 += timeUTCiso[i]; // milliseconds (not working in sg fetch url for some reason. maybe these are formatted incorrectly)
+  //}
+  //console.log(timeFormatted4); // milliseconds
+
+  /*
+function getLatLngFromMarkerClick() {
+  $('#map').on('click', marker[0], function() {
+    console.log(marker[0]);
+    //if (surfSpots[0].locationName === marker[0].getTitle()) {
+      console.log(marker[0].getTitle());
+      let lat = surfSpots[0].lat;
+      let lng = surfSpots[0].lng;
+      console.log(lat);
+      console.log(lng);
+    //}
+  });  
+}
+$(getLatLngFromMarkerClick);
+*/
